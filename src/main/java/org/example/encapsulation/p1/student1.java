d9b8dd07d15dac5fe952316fd53b670639e6e380
sravani <tsravani912@gmail.com>
2024-08-10 09:29:03 +0530

    constuctors

--- src/main/java/org/example/encapsulation/p1/student1.java
+++ src/main/java/org/example/encapsulation/p1/student1.java
@@ -10,4 +10,14 @@ public class student1 {
     void newbatch(){
         System.out.println(name+" is new to the batch");
     }
+    public student1(){
+       this.name="sravan";
+       this.psp=9;
+       System.out.println("i am inside no args constructor");
+    }
+    public student1(String name,int psp){
+        this.name=name;
+        this.psp=psp;
+        System.out.println("i am inside parametrized constructor");
+    }
 }
--- src/main/java/org/example/encapsulation/p2/main.java
+++ src/main/java/org/example/encapsulation/p2/main.java
@@ -4,8 +4,9 @@ import org.example.encapsulation.p1.student1;
 
 public class main {
     public static void main(String[] args) {
-        student1 s2=new student1();
-        s2.name="reddy";
-        s2.psp=00;
+        //student1 s2=new student1();
+        //s2.name="reddy";
+        //s2.psp=00;
+        student1 s3=new student1("lakshmi",10);
     }
 }
