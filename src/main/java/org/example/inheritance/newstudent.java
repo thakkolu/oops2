ae15fba00d7627cb6213cf6f6424eeab117e4da9
sravani <tsravani912@gmail.com>
2024-08-10 09:58:39 +0530

    inheritance

---
+++ src/main/java/org/example/inheritance/newstudent.java
@@ -0,0 +1,8 @@
+package org.example.inheritance;
+
+public class newstudent extends student{
+    public static void main(String[] args) {
+        student s1=new student();
+        s1.batch();
+    }
+}
---
+++ src/main/java/org/example/inheritance/student.java
@@ -0,0 +1,15 @@
+package org.example.inheritance;
+
+public class student {
+    int rollnumber;
+    String name;
+    int psp;
+     void batch(){
+        System.out.println("my name is "+name+". my roll number is "+rollnumber+". my psp is "+psp);
+    }
+    public student(){
+         this.rollnumber=30;
+         this.name="sravani";
+         this.psp=40;
+    }
+}
